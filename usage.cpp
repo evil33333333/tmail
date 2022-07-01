c81d07afe851daed619bf480700397f5ee0c7e55
Eve <84185604+evil33333333@users.noreply.github.com>
2022-07-01 06:51:24 +0100

    Create usage.cpp

---
+++ usage.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <iostream>
+#include <tmail/tmail.h>
+#include <tmail/inbox.h>
+
+int main()
+{
+    T_Mail t_mail;
+    T_Mail::Client* client = t_mail.MakeClient("email@mail.tm", "*******");
+    std::vector<std::string> verification_messages;
+    if (client->ok) 
+    {
+            std::cout << "[!] Created the email :)" << std::endl;
+        
+            // Get the current clients inbox
+            Inbox inbox = client->GetInbox();
+            if (inbox.ok)
+            {
+                for (auto const& msg : inbox.messages)
+                {
+                    if (msg.subject.find("verification code") != std::string::npos)
+                    {
+                        verification_messages.push_back(msg.subject);
+                    }
+                }
+            }
+    }
+    // Deletes the account from mail.tm and frees heap memory
+    t_mail.DeleteClient(client);
+    
+    
+    std::ofstream file("verification_codes.txt");
+    if (!file.is_open()) 
+    {
+        std::cout << "[X] Could not open a file :(" << std::endl;   
+    }
+    for (auto const& msg : verification_messages)
+    {
+        file << msg << std::endl;   
+    }
+    file.close();
+    
+    std::cout << "[!] Written all received verification messages to a file :)" << std::endl;
+    return 0;
+    
+    
+}
